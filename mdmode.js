4a40166f5d885eb46b28cb95f4a13f1a4d17a5f9
Takeshi Kunimi <takc923@gmail.com>
2013-11-14 01:49:17 +0900

    you can undo!

--- mdmode.js
+++ mdmode.js
@@ -1,6 +1,6 @@
 document.onreadystatechange = function () {
+    console.log(document.readyState);
     if (document.readyState != "complete") return;
-    console.log("loaded");
     var textareas = document.getElementsByTagName("textarea");
     for (var i = 0; i < textareas.length; i++) {
         textareas[i].addEventListener("keypress", mdmode);
@@ -15,21 +15,26 @@ function mdmode(evt) {
 
     if (evt.keyCode == 9) {
         var cursor = this.selectionStart;
-        var textArr = this.value.split("\n");
-        var lineIndex = getLineIndex(textArr, cursor);
-        var re = /^(\s*[*+-])/;
-        if (textArr[lineIndex].search(re) == -1) return;
-        textArr[lineIndex] = textArr[lineIndex].replace(re, "    $1");
-        this.value = textArr.join("\n");
+        var hol = getHeadOfLine(this.value, cursor);
+
+        if (hol == -1) return;
+
+        this.setSelectionRange(hol, hol);
+
+        var textEvent = document.createEvent("TextEvent");
+        textEvent.initTextEvent("textInput", true, true, null, "    ");
+        this.dispatchEvent(textEvent);
+
         this.setSelectionRange(cursor + 4, cursor + 4);
     }
 }
 
-function getLineIndex(textArr, cursor) {
-    var counter = 0;
-    for (var i = 0; i < textArr.length; i++) {
-        counter += textArr[i].length + 1; // 1 is linefeed
-        if (cursor < counter) return i;
+function getHeadOfLine(text, cursor) {
+    var re = /^[ \t]*[*+-].*$/gm;
+    for (var match = re.exec(text); re.lastIndex != 0; match = re.exec(text)) {
+        var hol = re.lastIndex - match[0].length;
+        var eol = re.lastIndex;
+        if (hol <= cursor && cursor <= eol) return hol;
     }
     return -1;
 }
